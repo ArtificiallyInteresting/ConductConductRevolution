20a555713497fd21af779e3ad78af2e883cded9f
Henry Smith <artificiallyinteresting@gmail.com>
2019-05-01 16:16:58 -0700

    It's working! I've got left/right/up/down working

--- motion.py
+++ motion.py
@@ -1,10 +1,36 @@
 import numpy as np
+import cv2
 
-def getMotions(image, oldImage, threshold):
+def getMotionMask(image, oldImage, threshold):
     difference = abs(oldImage.astype('int8') - image.astype('int8'))
     # return difference
     magnitudeDifference = np.linalg.norm(difference, axis=2)
     # changedMask = (magnitudeDifference * 255.0) / (np.max((magnitudeDifference)))
     changedMask = np.zeros_like(image)
     changedMask[magnitudeDifference > threshold] = 255.
+    cv2.imshow("motionImage", changedMask)
     return changedMask
+
+#top, bottom, left, right
+def getMotions(image, oldImage, thresholdChange, thresholdPercentage):
+    changedMask = getMotionMask(image, oldImage, thresholdChange)
+    width = image.shape[1]
+    height = image.shape[0]
+    size = width * height / 2
+    thresholdSize = size * thresholdPercentage
+    top = np.count_nonzero(changedMask[0:int(height/2)])
+    bottom = np.count_nonzero(changedMask[int(height/2):])
+    left = np.count_nonzero(changedMask[:,0:int(width/2)])
+    right = np.count_nonzero(changedMask[:,int(width/2):])
+    retVal = []
+    bestMotion = None
+    bestMotionScore = thresholdSize
+    motionScores = (top, bottom, left, right)
+    motionNames = ("top", "bottom", "left", "right")
+    for val in range(len(motionScores)):
+        motionScore = motionScores[val]
+        if (motionScore > bestMotionScore):
+            bestMotionScore = motionScore
+            bestMotion = motionNames[val]
+    return bestMotion
+
--- testMotion.py
+++ testMotion.py
@@ -3,6 +3,7 @@ import cv2
 from util import *
 import vision
 from motion import *
+import time
 
 def main():
     vc = startWebcamFeed()
@@ -11,13 +12,18 @@ def main():
     rval, oldFrame = vc.read()
     rval, frame = vc.read()
     width = 100
+    lastAction = time.time()
+    actionCooldown = 1.0
     while rval:
         oldFrameSegment = oldFrame[200:200+width,200:200+width].copy()
         frameSegment = frame[200:200+width,200:200+width].copy()
 
-        magnitudes = getMotions(oldFrameSegment, frameSegment, 100)
+        if lastAction + actionCooldown < time.time():
+            motion = getMotions(oldFrameSegment, frameSegment, 100, .6)
+            if motion is not None:
+                print(motion)
+                lastAction = time.time()
 
-        cv2.imshow("motionImage", magnitudes)
         oldFrame = frame.copy()
         cv2.rectangle(frame, (200,200), (200+width,200+width),  (255,255,0))
         showFrame(frame)
